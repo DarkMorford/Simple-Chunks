d9bfdfd6e60873810912360d0f95564bac083f7e
DarkMorford <DarkMorford@users.noreply.github.com>
2017-08-31 13:27:30 -0700

    Make the TE able to store a thing.

--- src/main/java/net/darkmorford/simplechunks/tileentity/TileEntityChunkLoader.java
+++ src/main/java/net/darkmorford/simplechunks/tileentity/TileEntityChunkLoader.java
@@ -1,7 +1,62 @@
 package net.darkmorford.simplechunks.tileentity;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class TileEntityChunkLoader extends TileEntity
 {
+	public static final int INV_SLOTS = 1;
+
+	private ItemStackHandler stackHandler = new ItemStackHandler(INV_SLOTS)
+	{
+		@Override
+		protected void onContentsChanged(int slot)
+		{
+			TileEntityChunkLoader.this.markDirty();
+		}
+	};
+
+	@Override
+	public void readFromNBT(NBTTagCompound compound)
+	{
+		super.readFromNBT(compound);
+		if (compound.hasKey("inventory"))
+		{
+			stackHandler.deserializeNBT((NBTTagCompound)compound.getTag("inventory"));
+		}
+	}
+
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound compound)
+	{
+		super.writeToNBT(compound);
+		compound.setTag("inventory", stackHandler.serializeNBT());
+		return compound;
+	}
+
+	@Override
+	public boolean hasCapability(Capability<?> capability, EnumFacing facing)
+	{
+		if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+		{
+			return true;
+		}
+
+		return super.hasCapability(capability, facing);
+	}
+
+	@Override
+	public <T> T getCapability(Capability<T> capability, EnumFacing facing)
+	{
+		if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+		{
+			return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.cast(stackHandler);
+		}
+
+		return super.getCapability(capability, facing);
+	}
 }

541d92ad4a0705dee7626068c288dfcdc8281117
DarkMorford <DarkMorford@users.noreply.github.com>
2017-10-10 16:52:34 -0700

    Fix block rendering in inventory.

--- src/main/java/net/darkmorford/simplechunks/block/BlockChunkLoader.java
+++ src/main/java/net/darkmorford/simplechunks/block/BlockChunkLoader.java
@@ -10,15 +10,20 @@ import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.ForgeChunkManager;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.Level;
 
 public class BlockChunkLoader extends Block implements ITileEntityProvider, TOPInfoProvider
@@ -29,6 +34,12 @@ public class BlockChunkLoader extends Block implements ITileEntityProvider, TOPI
 		setUnlocalizedName("chunkloader");
 		setRegistryName("chunkloader");
 		setCreativeTab(CreativeTabs.MISC);
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void initModel()
+	{
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
 	}
 
 	@Override

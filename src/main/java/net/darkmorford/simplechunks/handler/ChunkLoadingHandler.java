019f7c4802dc32241033873f2470bef73e5df0a3
DarkMorford <DarkMorford@users.noreply.github.com>
2017-10-11 18:29:56 -0700

    Properly load tickets on world load.

--- src/main/java/net/darkmorford/simplechunks/handler/ChunkLoadingHandler.java
+++ src/main/java/net/darkmorford/simplechunks/handler/ChunkLoadingHandler.java
@@ -1,11 +1,15 @@
 package net.darkmorford.simplechunks.handler;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
-import net.darkmorford.simplechunks.SimpleChunks;
+import net.darkmorford.simplechunks.tileentity.TileEntityChunkLoader;
+import net.minecraft.nbt.NBTUtil;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeChunkManager;
-import org.apache.logging.log4j.Level;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChunkLoadingHandler implements ForgeChunkManager.PlayerOrderedLoadingCallback
@@ -13,27 +17,42 @@ public class ChunkLoadingHandler implements ForgeChunkManager.PlayerOrderedLoadi
 	@Override
 	public void ticketsLoaded(List<ForgeChunkManager.Ticket> tickets, World world)
 	{
-		SimpleChunks.logger.info("ticketsLoaded callback fired");
-
 		for (ForgeChunkManager.Ticket ticket : tickets)
 		{
-			if (ticket.isPlayerTicket())
+			BlockPos ticketPosition = NBTUtil.getPosFromTag(ticket.getModData().getCompoundTag("position"));
+			TileEntity te = world.getTileEntity(ticketPosition);
+			if (te instanceof TileEntityChunkLoader)
 			{
-				SimpleChunks.logger.log(Level.INFO, "Releasing ticket for player " + ticket.getPlayerName());
+				TileEntityChunkLoader loader = (TileEntityChunkLoader) te;
+				loader.setChunkTicket(ticket);
+				loader.forceChunks();
 			}
-			else
-			{
-				SimpleChunks.logger.log(Level.INFO, "Releasing ticket for mod " + ticket.getModId());
-			}
-
-			ForgeChunkManager.releaseTicket(ticket);
 		}
 	}
 
 	@Override
 	public ListMultimap<String, ForgeChunkManager.Ticket> playerTicketsLoaded(ListMultimap<String, ForgeChunkManager.Ticket> tickets, World world)
 	{
-		// We don't care what order the tickets are in, just send back the same list
-		return tickets;
+		// We don't care what order the tickets are in, but filter out the invalid ones
+		ListMultimap<String, ForgeChunkManager.Ticket> validTickets = ArrayListMultimap.create();
+
+		for (String playerName : tickets.keySet())
+		{
+			List<ForgeChunkManager.Ticket> playerTickets = new ArrayList<>();
+
+			for (ForgeChunkManager.Ticket tkt : tickets.get(playerName))
+			{
+				BlockPos ticketPosition = NBTUtil.getPosFromTag(tkt.getModData().getCompoundTag("position"));
+				TileEntity te = world.getTileEntity(ticketPosition);
+				if (te instanceof TileEntityChunkLoader)
+				{
+					playerTickets.add(tkt);
+				}
+			}
+
+			validTickets.putAll(playerName, playerTickets);
+		}
+
+		return validTickets;
 	}
 }
